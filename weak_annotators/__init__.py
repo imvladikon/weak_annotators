8197e1d7c7056eac37514fe4fa6c0bec1a37c3f0
Vladimir Gurevich <vladimir@beaconcure.com>
2024-01-28 03:09:53 +0200

    init

--- weak_annotators/__init__.py
+++ weak_annotators/__init__.py
@@ -3,3 +3,4 @@
 from weak_annotators.flair_extractor import FlairExtractor
 from weak_annotators.medalpaca_extractor import MedAlpacaExtractor
 from weak_annotators.universal_ner_extractor import UniversalNerExtractor
+from weak_annotators.version import __version__
